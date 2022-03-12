1fc60a9ae2c5f901389d1ab6ce82ccf3b7025a75
digitaldags <janndaniel.dagusen@gmail.com>
2022-03-12 21:21:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/tahiti/tahiticore/TahitiCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.tahiti.tahiticore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TahitiCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<TahitiCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/tahiti/tahiticore/TahitiCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tahiti.tahiticore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TahitiCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
